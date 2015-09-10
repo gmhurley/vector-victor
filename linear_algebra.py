a929a953ab2e95949f6c0e74622f723fa25fbc89
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-09 21:04:34 -0400

    Formatting changes.

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,23 +1,13 @@
-"""
-Requirements
-
-Passing unit tests
-No use of third-party libraries - only built in + - / * operators and the math module
-No use of for or while loops
-"""
 from math import sqrt
 
 
 class ShapeException(Exception):
     pass
 
+shape_rule = "The vectors must be the same size."
+
 
 def shape(inp):
-    """
-    Takes a vector or matrix and return a tuple with the
-    number of rows (for a vector) or the number of rows and columns
-    (for a matrix.)
-    """
     try:
         y = len(inp[0])
         return (len(inp), y)
@@ -27,14 +17,14 @@ def shape(inp):
 
 def vector_add(x, y):
     if len(x) != len(y):
-        raise ShapeException("Shape rule: the vectors must be the same size.")
+        raise ShapeException(shape_rule)
     else:
         return [x[z] + y[z] for z in range(len(x))]
 
 
 def vector_sub(x, y):
     if len(x) != len(y):
-        raise ShapeException("Shape rule: the vectors must be the same size.")
+        raise ShapeException(shape_rule)
     else:
         return [x[z] - y[z] for z in range(len(x))]
 
@@ -42,14 +32,14 @@ def vector_sub(x, y):
 def vector_sum(*args):
     lst = [len(x) for x in args]
     if any(x != lst[0] for x in lst):
-        raise ShapeException("Shape rule: the vectors must be the same size.")
+        raise ShapeException(shape_rule)
     else:
         return [sum(x) for x in zip(*args)]
 
 
 def dot(x, y):
     if len(x) != len(y):
-        raise ShapeException("Shape rule: the vectors must be the same size.")
+        raise ShapeException(shape_rule)
     else:
         return sum([x[i] * y[i] for i in range(len(x))])
 
